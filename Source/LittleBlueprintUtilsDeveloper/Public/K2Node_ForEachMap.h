410c05e6bf411d71ca6bd4a867b27cbedf182133
AdamMambetov <adammambetov.342@gmail.com>
2026-03-02 19:47:35 +0300

    move map for each loop to developer module

---
+++ Source/LittleBlueprintUtilsDeveloper/Public/K2Node_ForEachMap.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "K2Node.h"
+#include "K2Node_ForEachMap.generated.h"
+
+
+UCLASS()
+class LITTLEBLUEPRINTUTILSDEVELOPER_API UK2Node_ForEachMap : public UK2Node
+{
+	GENERATED_BODY()
+
+protected:
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+
+	virtual void AllocateDefaultPins() override;
+
+	virtual void PostReconstructNode() override;
+
+	virtual FText GetMenuCategory() const override;
+
+	virtual void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
+
+	virtual FSlateIcon GetIconAndTint(FLinearColor& OutColor) const override;
+
+	virtual bool ShowPaletteIconOnNode() const override { return true; }
+
+	virtual void ExpandNode(
+		class FKismetCompilerContext& CompilerContext,
+		UEdGraph* SourceGraph) override;
+
+	virtual void NotifyPinConnectionListChanged(UEdGraphPin* Pin) override;
+};
